217fa8cfadf0347ecb7425714767e16e800dacfc
Bekmuhammad-Mamadiyev <bekmuhammadmamadiyev90@gmail.com>
2024-07-26 11:27:42 +0500

    main (common models add class)

--- common/models.py
+++ common/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+class Media(models.Model):
+    class MediaType(models.TextChoices):
+        IMAGE = "image", _("Image")
+        VIDEO = "video", _("Video")
+        MUSIC = "music", _("Music")
+        FILE = "file", _("File")
+    file = models.FileField(_('File'), upload_to="media/files")
+    type = models.CharField(_('Type'), max_length=60, choices=MediaType.choices)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.id} - {self.file.name}"
+
+class Settings(models.Model):
+    home_image = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True, blank=True)
+    home_title = models.CharField(_('Title'), max_length=120)
+    home_subtitle = models.CharField(_('Subtitle'), max_length=120)
+
+    def __str__(self):
+        return f"{self.id} - {self.home_title}"
+
+class Country(models.Model):
+    name = models.CharField(_('Name'), max_length=120)
+    code = models.CharField(_('Code'), max_length=2)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Region(models.Model):
+    name = models.CharField(_('Name'), max_length=120)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='regions')
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class OurInstagramStory(models.Model):
+    image = models.ForeignKey(Media, on_delete=models.CASCADE, related_name='instagram_stories')
+    story_link = models.URLField(_('Story Link'),)
+
+    def __str__(self):
+        return f"{self.id} - {self.story_link}"
+
+class CustomerFeedback(models.Model):
+    description = models.TextField(_('Review'),)
+    rank = models.IntegerField()
+    customer_name = models.CharField(_('Customer Name'),max_length=80)
+    customer_position = models.CharField(_('Customer Position'),max_length=60)
+    customer_image = models.ForeignKey(Media, on_delete=models.SET_NULL, null=True,)
+
+    def __str__(self):
+        return f"{self.id} - {self.customer_name}"
